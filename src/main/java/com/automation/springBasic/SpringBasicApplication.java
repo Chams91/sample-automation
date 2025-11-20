dc3d322e22fab128b7381db6ac2eafd1d16d8dad
Chamsedine Mansouri <chamsedine.mansouri@partner.digitalchargingsolutions.com>
2025-11-20 16:21:09 +0100

    import required deps

--- src/main/java/com/automation/springBasic/SpringBasicApplication.java
+++ src/main/java/com/automation/springBasic/SpringBasicApplication.java
@@ -2,12 +2,14 @@ package com.automation.springBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBasicApplication {
 
 	public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        SpringApplication.run(SpringBasicApplication.class, args);
     }
     
     @RestController
